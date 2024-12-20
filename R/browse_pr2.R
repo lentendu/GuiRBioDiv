a4b6f0fd57cafd75f8c8d85e8b9584fcdb069872
lentendu <glentendu@gmail.com>
2024-12-20 13:22:18 +0100

    Add partial match option to browse_pr2

--- R/browse_pr2.R
+++ R/browse_pr2.R
@@ -3,19 +3,37 @@
 #' Determine the taxonomic rank of the provided clade name in the PR2 database
 #'
 #' @param x a clade name
-#' @return a rank name or NA if not found
+#' @param partial allow partial match, TRUE by default
+#' @return a rank name for exact match, a named list for partial match(es) or NA if not found
 #'
 #' @references
 #' Guillou, L., Bachar, D., Audic, S., Bass, D., Berney, C., Bittner, L., Boutte, C. et al. 2013. The Protist Ribosomal Reference database (PR2): a catalog of unicellular eukaryote Small Sub-Unit rRNA sequences with curated taxonomy. Nucleic Acids Res. 41:D597–604.
 #'
 #' @export
 #'
-browse_pr2<- function(x) {
-  for (i in colnames(pr2database::pr2_taxonomy())[1:9]) {
-    if(length(grep(x,getElement(pr2database::pr2_taxonomy(),i)))){
+browse_pr2<- function(x, partial=TRUE) {
+  tmp_names<-colnames(pr2database::pr2_taxonomy())[1:9]
+  # exact match fist
+  for (i in tmp_names) {
+    if(any(which(x %in% getElement(pr2database::pr2_taxonomy(),i)))){
       return(i)
       break
     }
   }
-  return(NA)
+  # test partial match
+  if (partial) {
+    tmp_par<-lapply(tmp_names, function(i) {
+      tmp<-getElement(pr2database::pr2_taxonomy(),i)
+      unique(tmp[stringr::str_detect(tmp,x)])
+    })
+    names(tmp_par)<-tmp_names
+    tmp_par<-tmp_par[which(sapply(tmp_par,length)>0)]
+    if( length(tmp_par)>0 ) {
+      tmp_par
+    } else {
+      return(NA)
+    }
+  } else {
+    return(NA)
+  }
 }
--- tests/testthat/test-browse_pr2.R
+++ tests/testthat/test-browse_pr2.R
@@ -1,4 +1,5 @@
 test_that("browse_pr2 works", {
   expect_equal(browse_pr2("Ciliophora"), "subdivision")
-  expect_equal(browse_pr2("abc"),NA)
+  expect_equal(browse_pr2("abcde"),NA)
+  expect_equal(browse_pr2("Eukaryot",partial=F),NA)
 })
